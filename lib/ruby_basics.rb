8dc015501faa19cfb91eed0425edf6816ab99d39
monatru3 <monatru3@hotmail.com>
2015-10-05 02:08:53 -0400

    divine nine

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
-def assign_variable(value)
+def assign_variable(name)
+  name="Bob"
+end
+
+def argue(phrase)
+  phrase="I'm right and you are wrong!"
   
 end
 
-def argue
-  
+def greeting(greeting, name)
+  greeting="Hi there!"
+  name="Tom"
 end
 
-def greeting
-
-end
-
-def return_a_value
-
-end
-
-def last_evaluated_value
+def return_a_value(val="Nice")
+  return val
 
 end
 
-def pizza_party
+def last_evaluated_value(level="expert")
+return level
+end
 
+def pizza_party(type="cheese")
+return "#{type}"
 end
